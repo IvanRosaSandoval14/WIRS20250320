9801f922901111568f8a6855fe623128fb812fe5
Ivan Rosa Sandoval <User@WIN-JBH2C2BUVPP>
2025-03-20 08:00:56 -0600

    Migración de la base de datos al código

---
+++ WIRS20250320.AppWebMVC/Models/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WIRS20250320.AppWebMVC.Models;
+
+public partial class Warehouse
+{
+    public int WarehouseId { get; set; }
+
+    public string WarehouseName { get; set; } = null!;
+
+    public string? Notes { get; set; }
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
